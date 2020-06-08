727e5b9f50ab44ce1b253849058351e3d991557d
Henry Mukherji <hlm27@duke.edu>
2020-06-08 14:51:12 -0400

    add 'you lost' caption

--- game.py
+++ game.py
@@ -2,8 +2,6 @@ import random, sys
 import numpy as np
 import pygame as pg
 
-
-#random.seed(1)
 HEIGHT = 200 #window dimensions
 WIDTH = 200
 GREY = (128,128,128) #rgb value
@@ -14,6 +12,7 @@ GREEN = (0, 128, 0)
 PURPLE = (128, 0, 128)
 
 random.seed(1)
+
 def init_board():
     board = [[0] * 10 for _ in range(10)]
     for i in range(10):
@@ -59,7 +58,6 @@ def init_nums():
     print(np.array(board))
     return board
 
-
 def create_grid():
     board = init_nums()
     #print(board)
@@ -69,35 +67,31 @@ def create_grid():
     win.fill(GREY)
     pg.display.set_caption("Minesweeper")
     font = pg.font.SysFont('Arial', 25)
-
+    IMAGE = pg.image.load("pngflow.com.png")
+    rect2 = IMAGE.get_rect()
+    clickable = True
     done = False
+
     while not done:
         for x in range(10):
             for y in range(10):
                 rectColor = WHITE
                 rect = pg.Rect(y * square_size, x * square_size, square_size, square_size)
                 pg.draw.rect(win, rectColor, rect, 1)
+
         for event in pg.event.get():
-            if event.type == pg.MOUSEBUTTONDOWN:
+            if clickable and event.type == pg.MOUSEBUTTONDOWN:
                 Mouse_x, Mouse_y = pg.mouse.get_pos()
-                # print("Not found" + str(Mouse_x) + " " + str(Mouse_y))
-                # print(Mouse_//20,Mouse_y//20)
-                # print(board[Mouse_x//20][Mouse_y//20])
                 if board[Mouse_y //20 ][Mouse_x//20] == 1:
-                    print(Mouse_y//20, Mouse_x//20)
-                    print(Mouse_y,Mouse_x)
                     win.blit(font.render('1', True, RED),(Mouse_x//20 * 20, Mouse_y//20 * 20) )
                 if board[Mouse_y//20 ][Mouse_x//20] == 2:
-                    print(Mouse_y//20, Mouse_x//20)
-                    print(Mouse_y,Mouse_x)
                     win.blit(font.render('2', True, BLUE),(Mouse_x//20 * 20, Mouse_y//20 * 20) )
                 if board[Mouse_y //20 ][Mouse_x//20] == 3:
-                    print(Mouse_y//20, Mouse_x//20)
-                    print(Mouse_y,Mouse_x)
                     win.blit(font.render('3', True, PURPLE),(Mouse_x//20 * 20, Mouse_y//20 * 20) )
-                if board[x][y]==9:
-                    rectColor=RED
-                    pg.draw.rect(win, rectColor, rect, 0)
+                if board[Mouse_y //20 ][Mouse_x//20] == 9:
+                    win.blit(IMAGE, (Mouse_x//20 * 20, Mouse_y//20 * 20), rect2)
+                    clickable= False
+                    pg.display.set_caption("You lost!")
             if event.type == pg.QUIT:
                 done = True
         pg.display.update() #update display
