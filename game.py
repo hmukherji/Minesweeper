afa3c77fe0f5caf1491a1d4ffcd0ebc415cdef68
Henry Mukherji <hlm27@duke.edu>
2020-06-08 15:01:53 -0400

    simplify blit and add colors dict

--- game.py
+++ game.py
@@ -10,12 +10,14 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 128, 0)
 PURPLE = (128, 0, 128)
+BOARD_SIZE = 10
 
+colors = {1:RED, 2:BLUE, 3:GREEN}
 random.seed(1)
 
 def init_board():
-    board = [[0] * 10 for _ in range(10)]
-    for i in range(10):
+    board = [[0] * 10 for _ in range(BOARD_SIZE)]
+    for i in range(BOARD_SIZE):
         row = random.randint(1, 9)
         column = random.randint(1, 9)
         board[row][column] = 9
@@ -62,7 +64,7 @@ def create_grid():
     board = init_nums()
     #print(board)
     pg.init()
-    square_size = 20
+    SQUARE_SIZE = 20
     win = pg.display.set_mode((HEIGHT, WIDTH))
     win.fill(GREY)
     pg.display.set_caption("Minesweeper")
@@ -76,18 +78,15 @@ def create_grid():
         for x in range(10):
             for y in range(10):
                 rectColor = WHITE
-                rect = pg.Rect(y * square_size, x * square_size, square_size, square_size)
+                rect = pg.Rect(y * SQUARE_SIZE, x * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
                 pg.draw.rect(win, rectColor, rect, 1)
 
         for event in pg.event.get():
             if clickable and event.type == pg.MOUSEBUTTONDOWN:
                 Mouse_x, Mouse_y = pg.mouse.get_pos()
-                if board[Mouse_y //20 ][Mouse_x//20] == 1:
-                    win.blit(font.render('1', True, RED),(Mouse_x//20 * 20, Mouse_y//20 * 20) )
-                if board[Mouse_y//20 ][Mouse_x//20] == 2:
-                    win.blit(font.render('2', True, BLUE),(Mouse_x//20 * 20, Mouse_y//20 * 20) )
-                if board[Mouse_y //20 ][Mouse_x//20] == 3:
-                    win.blit(font.render('3', True, PURPLE),(Mouse_x//20 * 20, Mouse_y//20 * 20) )
+                if board[Mouse_y //20 ][Mouse_x//20] != 9 and board[Mouse_y //20 ][Mouse_x//20] != 0:
+                   render_num(win,font, board[Mouse_y //20 ][Mouse_x//20],
+                              colors[board[Mouse_y //20][Mouse_x//20]], Mouse_x, Mouse_y)
                 if board[Mouse_y //20 ][Mouse_x//20] == 9:
                     win.blit(IMAGE, (Mouse_x//20 * 20, Mouse_y//20 * 20), rect2)
                     clickable= False
@@ -98,6 +97,10 @@ def create_grid():
 
     pg.quit()
     sys.exit()
+
+def render_num(screen, font, num, color, mouse_x, mouse_y):
+    screen.blit(font.render(str(num), True, color), (mouse_x // 20 * 20, mouse_y // 20 * 20))
+
 
 if __name__ == "__main__":
     create_grid()
